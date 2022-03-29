d6d77066839d69fcb82154a9b2960dd74da62fee
shenna12 <94024272+shenna12@users.noreply.github.com>
2022-03-29 15:47:26 +0700

    Add files via upload

---
+++ ukk_shena/admin/config/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('baseurl','http://localhost/ukk_shena/public');
+
+//DB
+define('DB_HOST','lcalhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','portofolio_shenna');
---
+++ ukk_shena/admin/controllers/Portofolio.php
@@ -0,0 +1,9 @@
+<?php
+
+class Portofolio extends Controller
+{
+    public function index()
+    {
+        echo "Ini controller portofolio";
+    }
+}
